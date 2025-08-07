2cca82f10c1f02b3698c00a06e7da72670cd6a2e
Thiago Poliseli <thiagopoliseli@gmail.com>
2025-08-07 11:05:16 -0300

    Desenvolvimento do ecommerce

---
+++ next.config.ts
@@ -0,0 +1,13 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "d4lgxe9bm8juw.cloudfront.net",
+      },
+    ],
+  },
+};
+export default nextConfig;
